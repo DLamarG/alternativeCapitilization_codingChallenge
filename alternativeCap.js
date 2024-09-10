7bef9daffdb974294758fcef087d1b25f16f91ad
DLamarG <demondg8@gmail.com>
2024-09-10 13:02:35 -0500

    added code

---
+++ alternativeCap.js
@@ -0,0 +1,22 @@
+function capitalize(s){
+    let resultRR = [];
+    let count = 0;
+    while(count <= 2){
+      count++
+      charRR = s.split('')
+      if(count===1){
+        newStr = charRR.map((x,index)=> index%2==0 ? x.toUpperCase() : x.toLowerCase()).join('')
+        resultRR.push(newStr)
+      }
+      if(count===2){
+        newStr = charRR.map((x,index)=> index%2==0 ? x.toLowerCase() : x.toUpperCase()).join('')
+        resultRR.push(newStr)
+      }
+    }
+    return resultRR
+  };
+
+
+console.log(capitalize())
+console.log(capitalize())
+console.log(capitalize())
